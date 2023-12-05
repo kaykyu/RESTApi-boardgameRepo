5e48f591520c619e4bc525176a53c05af3c6598c
kaykyu <kaiqing.97@outlook.com>
2023-12-05 23:38:19 +0800

    initial commit

--- src/main/java/kq/practice/ssf16workshop/controller/ApiController.java
+++ src/main/java/kq/practice/ssf16workshop/controller/ApiController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
